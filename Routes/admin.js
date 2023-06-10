29b87ef1552a83a1c213ce2f66cd399ebc7c8414
Muzi09 <amzikhan711@gmail.com>
2023-06-10 14:37:55 +0530

    Minor Changes

--- Routes/admin.js
+++ Routes/admin.js
@@ -219,4 +219,33 @@ admin.get('/subjectName/:id', async (req, res) => {
 
 
 
+
+admin.get('/subjectName/:id', async (req, res) => {
+  try {
+    const subjectId = req.params.id;
+    const foundSubject = await subject.findById(subjectId);
+
+    if (!foundSubject) {
+      return res.status(404).json({
+        message: 'Subject not found',
+      });
+    }
+
+    res.status(200).json({
+      data: foundSubject.name,
+      message: 'Successfully fetched the subject name',
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+
+
+
+
+
+
 module.exports = admin
--- Routes/users.js
+++ Routes/users.js
@@ -225,6 +225,34 @@ users.post('/signup', async (req, res) => {
 
 
 
+users.get('/subject/:subjectId/questions', async (req, res) => {
+  try {
+    const subjectId = req.params.subjectId
+
+    const foundSubject = await subject.findById(subjectId)
+
+    if (!foundSubject) {
+      return res.status(404).json({
+        message: 'Subject not found'
+      })
+    }
+
+    const questions = await question.find({ subjectID: subjectId })
+
+    res.status(200).json({
+      data: questions,
+      message: 'Questions fetched successfully'
+    })
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
+
+
+
+
 
 
 
